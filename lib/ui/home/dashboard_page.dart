be6b2393baa8ac19e6011102434868faef75d025
Ronitrs5 <114333103+Ronitrs5@users.noreply.github.com>
2023-09-01 21:41:52 +0530

    Dashboard inserted fixes

--- lib/ui/home/dashboard_page.dart
+++ lib/ui/home/dashboard_page.dart
@@ -11,8 +11,36 @@ class _DashboardPageState extends State<DashboardPage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
+        backgroundColor: Colors.blueAccent,
         title: const Text("Dashboard"),
-      )
+      ),
+
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+          AspectRatio(
+            aspectRatio: 1.0,
+            child: SizedBox(
+              width: double.infinity,
+              child: Card(
+              elevation: 4, // Controls the shadow of the card
+              margin: const EdgeInsets.all(16), // Margin around the card
+              child: Container(
+                padding: const EdgeInsets.all(16), // Padding inside the card
+                child: const Text(
+                  'Collection',
+                  style: TextStyle(
+                    fontSize: 20,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+              ),
+        ),
+            ),
+          ),
+          ],
+        ),
+      ),
     );
   }
 }
